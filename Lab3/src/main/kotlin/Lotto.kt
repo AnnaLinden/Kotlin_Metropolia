46854d3bda8280d084912c7fc45e219e77d2787e
AnnaLinden <linden.a.anna@gmail.com>
2024-08-27 20:36:47 +0300

    lab3

---
+++ Lab3/src/main/kotlin/Lotto.kt
@@ -0,0 +1,38 @@
+class Lotto(private val n: Int, private val lottoRange: IntRange, secretNumbers: List<Int>? = null) {
+    private val secretNumbers = secretNumbers  ?: pickNDistinct(lottoRange, n)
+    // returns a list with n distinct ints from range
+    fun pickNDistinct(range: IntRange, n: Int): List<Int>? {
+        if(n>range.count()) return null
+        val pickedNumbers = mutableSetOf<Int>()
+        while (pickedNumbers.size <n ){
+            val randomNum = range.random()
+            pickedNumbers.add(randomNum)
+        }
+        return pickedNumbers.toList()
+    }
+
+    // return the number of distinct ints in list
+    fun numDistinct(list: List<Int>): Int {
+        return list.toSet().size
+    }
+
+    // return the number of ints in both list1 and list2
+    fun numCommon(list1: List<Int>, list2: List<Int>): Int {
+        return list1.intersect(list2).size
+    }
+
+    // is guess legal? (consists of n different ints from range)
+    fun isLegalLottoGuess(guess: List<Int>, range: IntRange = lottoRange, count: Int = n): Boolean {
+        return guess.toSet().size == count && guess.all {it in range}
+    }
+
+    // if guess is legal return the number of ints in guess that also appear in secret, otherwise 0
+    fun checkGuess(guess: List<Int>, secret: List<Int> = secretNumbers): Int {
+        return if (isLegalLottoGuess(guess)) numCommon(guess, secret) else 0
+    }
+
+
+
+
+
+}
